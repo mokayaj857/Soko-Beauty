c3747b2c75da2c5ccf34c8e50fac00e973e1aa23
PETER IRUNGU <peteralidante254@gmail.com>
2024-06-03 20:03:02 +0300

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-import 'package:soko_beauty/config/theme/themes/dark.dart';
-import 'package:soko_beauty/config/theme/themes/light.dart';
+// import 'package:soko_beauty/config/theme/themes/dark.dart';
+// import 'package:soko_beauty/config/theme/themes/light.dart';
 import 'feautures/auth/views/services/user_provider.dart';
 import 'firebase_options.dart';
 import 'home/views/screens/splashscreen.dart';
@@ -41,27 +41,12 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    
-    Brightness brightness = Theme.of(context).brightness;
-
-    Color systemNavigationBarColor =
-        brightness == Brightness.dark ? Colors.black : Colors.white;
-    Brightness statusBarIconBrightness =
-        brightness == Brightness.dark ? Brightness.light : Brightness.dark;
-    Brightness statusBarBrightness =
-        brightness == Brightness.dark ? Brightness.light : Brightness.dark;
-
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
-      systemNavigationBarIconBrightness: brightness,
-      systemNavigationBarColor: systemNavigationBarColor,
-      statusBarIconBrightness: statusBarIconBrightness,
-      statusBarBrightness: statusBarBrightness,
-      systemStatusBarContrastEnforced: false,
     ));
     return MaterialApp(
-      theme: lightMode,
-      darkTheme: darkMode,
+      // theme: lightMode,
+      // darkTheme: darkMode,
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
     );
